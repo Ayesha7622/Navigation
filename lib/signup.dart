fbcfe7825e3aa431f13e34d7899afb1d414c5832
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-17 23:31:05 +0500

    ddjcnbdsm

--- lib/signup.dart
+++ lib/signup.dart
@@ -11,23 +11,24 @@ class Signup extends StatefulWidget {
 class _SignupState extends State<Signup> {
   TextEditingController firstController = TextEditingController();
   TextEditingController lastController = TextEditingController();
+  TextEditingController phonenoController = TextEditingController();
+  TextEditingController cnicController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  TextEditingController phonenoController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 114, 111, 111),
         appBar: AppBar(
-          leading: Icon(
-            Icons.login,
-            size: 40,
-            color: Colors.white,
-          ),
+          // leading: Icon(
+          //   Icons.,
+          //   size: 40,
+          //   color: Colors.white,
+          // ),
           backgroundColor: const Color.fromARGB(255, 71, 73, 76),
           title: Text(
-            'Login Screen',
+            'SignUp Screen',
             style: TextStyle(
                 color: Colors.white, fontSize: 45, fontWeight: FontWeight.bold),
           ),
@@ -55,7 +56,7 @@ class _SignupState extends State<Signup> {
                       TextStyle(color: const Color.fromARGB(255, 96, 84, 84)),
                   decoration: InputDecoration(
                     icon: Icon(Icons.email),
-                    hintText: "Enter Email:",
+                    hintText: "Enter First_Name:",
                     hintStyle:
                         TextStyle(color: const Color.fromARGB(255, 21, 21, 21)),
                   ),
@@ -78,7 +79,99 @@ class _SignupState extends State<Signup> {
                       TextStyle(color: const Color.fromARGB(255, 105, 94, 94)),
                   decoration: InputDecoration(
                     icon: Icon(Icons.password),
-                    hintText: "Enter Password:",
+                    hintText: "Enter Last_Name:",
+                    hintStyle:
+                        TextStyle(color: const Color.fromARGB(255, 18, 17, 17)),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 88, vertical: 30),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: passwordController,
+                  style:
+                      TextStyle(color: const Color.fromARGB(255, 105, 94, 94)),
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.password),
+                    hintText: "Enter Phone_No:",
+                    hintStyle:
+                        TextStyle(color: const Color.fromARGB(255, 18, 17, 17)),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 88, vertical: 30),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: passwordController,
+                  style:
+                      TextStyle(color: const Color.fromARGB(255, 105, 94, 94)),
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.password),
+                    hintText: "Enter CNIC:",
+                    hintStyle:
+                        TextStyle(color: const Color.fromARGB(255, 18, 17, 17)),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 88, vertical: 30),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: passwordController,
+                  style:
+                      TextStyle(color: const Color.fromARGB(255, 105, 94, 94)),
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.password),
+                    hintText: "Enter Email:",
+                    hintStyle:
+                        TextStyle(color: const Color.fromARGB(255, 18, 17, 17)),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 88, vertical: 30),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: passwordController,
+                  style:
+                      TextStyle(color: const Color.fromARGB(255, 105, 94, 94)),
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.password),
+                    hintText: "Password:",
                     hintStyle:
                         TextStyle(color: const Color.fromARGB(255, 18, 17, 17)),
                   ),
