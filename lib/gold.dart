994fb8d2a5c097c8e6192c5b0097fc34f3e0bdff
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-22 14:00:28 +0500

    hcvdhn

--- lib/gold.dart
+++ lib/gold.dart
@@ -18,9 +18,11 @@ class _GoldState extends State<Gold> {
       TextEditingController();
   TextEditingController quantity_of_tola_goldcontroller =
       TextEditingController();
-  TextEditingController amount_of_tola_pricecontroller =
+  TextEditingController quantity_of_masha_goldcontroller =
       TextEditingController();
-  TextEditingController amount_of_tola_pricecontroller =
+  TextEditingController quantity_of_ratti_goldcontroller =
+      TextEditingController();
+  TextEditingController quantity_of_point_goldcontroller =
       TextEditingController();
   @override
   Widget build(BuildContext context) {
