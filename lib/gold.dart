dff56ff34096bd36df268e7ec21036fd35ab3e2e
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-23 18:46:12 +0500

    jbvjf

--- lib/gold.dart
+++ lib/gold.dart
@@ -16,6 +16,8 @@ class _GoldState extends State<Gold> {
   double priceMasha = 0;
   double priceratti = 0;
   double pricePoint = 0;
+  int totalPtice = 0;
+
   // double total_masha_price = 0;
   // double total_ratti_price = 0;
   // double total_point_price = 0;
@@ -32,16 +34,16 @@ class _GoldState extends State<Gold> {
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          FloatingActionButton(
-              shape: CircleBorder(),
-              onPressed: () {
-                priceOfTolaController.clear();
-                quantityOfTolagoldController.clear();
-              }),
-          SizedBox(
-            width: 10,
-          ),
-          FloatingActionButton(onPressed: () {})
+          // FloatingActionButton(
+          //     shape: CircleBorder(),
+          //     onPressed: () {
+          //       priceOfTolaController.clear();
+          //       quantityOfTolagoldController.clear();
+          //     }),
+          // SizedBox(
+          //   width: 10,
+          // ),
+          // FloatingActionButton(onPressed: () {})
         ],
       ),
       backgroundColor: const Color.fromARGB(255, 20, 20, 20),
@@ -49,26 +51,31 @@ class _GoldState extends State<Gold> {
         backgroundColor: Colors.amber,
         title: Text(
           'Jwellery App',
-          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
+        leading: Icon(Icons.calculate),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
+            SizedBox(
+              height: 20,
+            ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 5),
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.amber,
-                  borderRadius: BorderRadius.circular(15),
+                  borderRadius: BorderRadius.circular(10),
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
+                    keyboardType: TextInputType.number,
                     controller: priceOfTolaController,
-                    style:
-                        TextStyle(color: const Color.fromARGB(255, 24, 23, 23)),
+                    style: TextStyle(color: Colors.yellow),
                     decoration: InputDecoration(
+                        border: InputBorder.none,
                         hintText: 'Price Of Tola=',
                         hintStyle: TextStyle(color: Colors.black)),
                   ),
@@ -85,27 +92,29 @@ class _GoldState extends State<Gold> {
             //   ),
             // ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 5),
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.amber,
-                  borderRadius: BorderRadius.circular(15),
-                ),
+                    //color: Colors.amber,
+                    border: Border.all(width: 2, color: Colors.amber),
+                    borderRadius: BorderRadius.circular(10)),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
+                    keyboardType: TextInputType.number,
                     controller: quantityOfTolagoldController,
                     style:
                         TextStyle(color: const Color.fromARGB(255, 15, 15, 15)),
                     decoration: InputDecoration(
+                        border: InputBorder.none,
                         hintText: 'Quantity Of Tola gold=',
-                        hintStyle: TextStyle(color: Colors.black)),
+                        hintStyle: TextStyle(color: Colors.yellow)),
                   ),
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 5),
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.amber,
@@ -125,18 +134,18 @@ class _GoldState extends State<Gold> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 5),
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.white,
+                  color: Colors.amber,
                   borderRadius: BorderRadius.circular(15),
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
                     controller: quantityOfRattiGoldcontroller,
-                    style: TextStyle(
-                        color: const Color.fromARGB(255, 231, 228, 228)),
+                    style:
+                        TextStyle(color: const Color.fromARGB(255, 10, 10, 10)),
                     decoration: InputDecoration(
                         hintText: 'Quantity Of Ratti gold=',
                         hintStyle: TextStyle(color: Colors.black)),
@@ -145,18 +154,17 @@ class _GoldState extends State<Gold> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 5),
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.white,
+                  color: Colors.amber,
                   borderRadius: BorderRadius.circular(15),
                 ),
                 child: Padding(
-                  padding: const EdgeInsets.all(0.0),
+                  padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
                     controller: quantityOfPointGoldcontroller,
-                    style: TextStyle(
-                        color: const Color.fromARGB(255, 231, 228, 228)),
+                    style: TextStyle(color: const Color.fromARGB(255, 7, 7, 7)),
                     decoration: InputDecoration(
                         hintText: 'Quantity Of Point gold=',
                         hintStyle: TextStyle(color: Colors.black)),
@@ -164,78 +172,136 @@ class _GoldState extends State<Gold> {
                 ),
               ),
             ),
-            FloatingActionButton(
-                onPressed: () {
-                  priceOfTola = int.parse(priceOfTolaController.text);
-                  quantityOfTolaGold =
-                      int.parse(quantityOfTolagoldController.text);
-                  quantityOfMashaGold =
-                      int.parse(quantityOfMashaGoldController.text);
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                    backgroundColor: Colors.amber,
+                    onPressed: () {
+                      priceOfTola = int.parse(priceOfTolaController.text);
+                      quantityOfTolaGold =
+                          int.parse(quantityOfTolagoldController.text);
+                      quantityOfMashaGold =
+                          int.parse(quantityOfMashaGoldController.text);
 
-                  quantityOfRattiGold =
-                      int.parse(quantityOfRattiGoldcontroller.text);
-                  quantityOfPointGold =
-                      int.parse(quantityOfPointGoldcontroller.text);
-                  if (priceOfTola == "" &&
-                      quantityOfTolaGold == "" &&
-                      quantityOfMashaGold == "" &&
-                      quantityOfRattiGold == "" &&
-                      quantityOfPointGold == "") {
-                    priceMasha = quantityOfTolaGold / 12;
+                      quantityOfRattiGold =
+                          int.parse(quantityOfRattiGoldcontroller.text);
+                      quantityOfPointGold =
+                          int.parse(quantityOfPointGoldcontroller.text);
+                      // if (priceOfTola == "" &&
+                      //     quantityOfTolaGold == "" &&
+                      //     quantityOfMashaGold == "" &&
+                      //     quantityOfRattiGold == "" &&
+                      //     quantityOfPointGold == "") {
+                      priceMasha = quantityOfTolaGold / 12;
 
-                    double totalMashaPrice = priceMasha * quantityOfMashaGold;
-                    priceratti = quantityOfMashaGold / 96;
+                      double totalMashaPrice = priceMasha * quantityOfMashaGold;
+                      priceratti = quantityOfMashaGold / 96;
 
-                    double total_ratti_price = priceratti * quantityOfRattiGold;
-                    pricePoint = quantityOfPointGold / 300;
-                    double totalPointPrice = pricePoint * quantityOfPointGold;
+                      double totalRattiPrice = priceratti * quantityOfRattiGold;
+                      pricePoint = quantityOfPointGold / 300;
+                      double totalPointPrice = pricePoint * quantityOfPointGold;
 
-                    priceOfTola = (priceOfTola * quantityOfTolaGold) +
-                        quantityOfMashaGold +
-                        quantityOfRattiGold +
-                        quantityOfPointGold;
-                    setState(() {});
-                  }
-                },
-                child:
-                    //Icon(Icons.calculate);
-                    Text('Calculate')),
+                      totalPtice = (priceOfTola * quantityOfTolaGold) +
+                          quantityOfMashaGold +
+                          quantityOfRattiGold +
+                          quantityOfPointGold;
+                      print('Total Price=$totalPtice');
+                      setState(() {});
+                      //}
+                    },
+                    child: Icon(Icons.calculate)
+                    //     Text(
+                    //   'Calculate',
+                    // )
+                    ),
+                SizedBox(width: 20),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.amber),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Calculatr',
+                        style: TextStyle(color: Colors.black),
+                      )),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                FloatingActionButton(
+                    backgroundColor: Colors.amber,
+                    onPressed: () {
+                      setState(() {});
+                    },
+                    child: Icon(Icons.refresh) //Text('Clear'),
+                    ),
+              ],
+            ),
+            // FloatingActionButton(
+            //   onPressed: () {},
+            //   child: Text('Clear'),
+            // ),
+
+            // FloatingActionButton(
+            //   onPressed: () {},
+            //   child: Text('Clear'),
+            // ),
             SizedBox(
               height: 30,
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Total Price Of Tola ,',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                'Total Price Of Rs.$totalPtice',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Quantity of Tola Gold ,',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                'Quantity of Tola Gold =',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Quantity of Masha Gold ,',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                'Quantity of Masha Gold =',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Quantity of Ratti gold ,',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                'Quantity of Ratti gold =',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Quantity of POint Gold ,',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                'Quantity of POint Gold =',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber),
               ),
             ),
           ],
